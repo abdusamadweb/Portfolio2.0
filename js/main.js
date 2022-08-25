36d61517ed3fb4e1584b3011b96079ef15bb3e9a
abdusamadweb <abdusamadweb@gmail.com>
2022-08-25 16:52:51 +0500

    bugss :/

--- js/main.js
+++ js/main.js
@@ -3,21 +3,12 @@ let elModal = document.querySelector(".nav");
 let elLinethird = document.querySelector(".line2");
 let elLineOne = document.querySelector(".line1");
 let elLineThree = document.querySelector(".line3");
-let elLine = document.querySelectorAll(".experience__line");
 
 elMenuBtn.addEventListener("click", function () {
   elModal.classList.toggle("grow-height");
   elLinethird.classList.toggle("remove-line");
   elLineOne.classList.toggle("rotate-line1");
   elLineThree.classList.toggle("rotate-line3");
-});
-
-elLine.forEach((item) => {
-  window.addEventListener("scroll", function () {
-    header = document.querySelector("header");
-    header.classList.toggle("header-bg", window.scrollY > 200);
-    item.classList.toggle("float", window.scrollY > 300);
-  });
 });
 
 window.addEventListener('scroll', () => {
@@ -30,10 +21,16 @@ const sections = document.querySelectorAll("section");
 function changeLinkState() {
   let index = sections.length;
 
-  while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
+  while (--index && window.scrollY + 71.5 < sections[index].offsetTop) {}
 
   links.forEach((link) => link.classList.remove("nav__link--active"));
   links[index].classList.add("nav__link--active");
+}
+
+for(let i of links) {
+  i.addEventListener('click', () => {
+    elModal.classList.remove('grow-height');
+  })
 }
 
 changeLinkState();

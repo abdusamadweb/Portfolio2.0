260681840ab473fe346e3f19bdb62c1fa036c600
abdusamadweb <abdusamadweb@gmail.com>
2022-08-25 15:59:46 +0500

    we change repository and change some bugs

---
+++ js/data.js
@@ -0,0 +1,203 @@
+const data = {
+  skills: ['GIT', 'HTML5', 'CSS3', 'Bootstrap5', 'Java Script', 'ReactJs', 'Redux', 'Redux Toolkit', 'Material UI', 'Vue', 'VueX', 'Composition API'],
+  offers: [
+    {
+      id: 1,
+      img: '../img/offer1.png',
+      title: 'REACT DEVELOPER',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, hic.'
+    },
+    {
+      id: 2,
+      img: '../img/offer2.png',
+      title: 'WEB DESIGN',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, hic.'
+    },
+    {
+      id: 3,
+      img: '../img/offer3.png',
+      title: 'VUE DEVELOPER',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, hic.'
+    },
+    {
+      id: 4,
+      img: '../img/offer4.png',
+      title: 'CODER',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, hic.'
+    },
+  ],
+  awards: [
+    {
+      icon: `fas fa-bell-plus`,
+      count: 30,
+      desc: 'PROJECT COMPLETED'
+    },
+    {
+      icon: `fas fa-laugh-beam`,
+      count: 1,
+      desc: 'HAPPY CUSTOMMERS'
+    },
+    {
+      icon: `fas fa-binoculars`,
+      count: 122,
+      desc: 'CUPS OF COFFEE'
+    },
+    {
+      icon: `fas fa-pen`,
+      count: 1,
+      desc: 'AWARD WINNIG'
+    },
+  ],
+  allproducts: [
+    {
+      id: 1,
+      img: '../img/elessi.png',
+      title: 'ELESSI SHOP',
+      github: 'https://github.com/abudamadweb/Elessi-Shop',
+      link: 'https://elessi-byabdusamad.netlify.app/',
+      category: 'react'
+    },
+    {
+      id: 2,
+      img: '../img/kiwi.png',
+      title: 'KIWI SHOP',
+      github: 'https://compassionate-wing-2b700d.netlify.app/',
+      link: 'https://github.com/MrDeath22/Kivi-magazin',
+      category: 'react'
+    },
+    {
+      id: 3,
+      img: '../img/product.jpg',
+      title: 'Product',
+      github: 'https://github.com/abdusamadweb/Product',
+      link: 'https://product-byabdusamad.netlify.app/',
+      category: 'html'
+    },
+    {
+      id: 4,
+      img: '../img/freshio.png',
+      title: 'FRESHIO',
+      github: 'https://github.com/abdusamadweb/Freshio',
+      link: 'https://freshio-byabdusamad.netlify.app/',
+      category: 'html'
+    },
+    {
+      id: 5,
+      img: '../img/music-player.png',
+      title: 'MUSIC PLAYER',
+      github: 'https://github.com/abdusamadweb/Music-Player',
+      link: 'https://musing-neumann-30aaef.netlify.app/',
+      category: 'react'
+    },
+    {
+      id: 6,
+      img: '../img/fresh2.png',
+      title: 'fresh',
+      github: 'https://github.com/abdusamadweb/Fresh1',
+      link: 'https://fresh1by-abdusamad.netlify.app/',
+      category: 'html'
+    },
+    {
+      id: 7,
+      img: '../img/finswet.jpg',
+      title: 'finswet',
+      github: 'https://github.com/abdusamadweb/finsweet-myteam',
+      link: 'https://finsweet-byabdusamad.netlify.app/',
+      category: 'html'
+    },
+    {
+      id: 8,
+      img: '../img/portfolio.png',
+      title: 'portfolio',
+      github: 'https://github.com/abdusamadweb/Portfolio',
+      link: 'https://portfolio-byabdusamad.netlify.app/',
+      category: 'html'
+    },
+    {
+      id: 9,
+      img: '../img/qashqadaryo.png',
+      title: 'Qashqadaryo',
+      github: 'https://github.com/abdusamadweb/Qashqadaryo-boshqarmasi',
+      link: 'https://qashqadaryo-byabdusamad.netlify.app/',
+      category: 'vue'
+    },
+  ],
+  react: [
+    {
+      id: 1,
+      img: '../img/elessi.png',
+      title: 'ELESSI SHOP',
+      github: 'https://github.com/abudamadweb/Elessi-Shop',
+      link: 'https://elessi-byabdusamad.netlify.app/',
+      category: 'react'
+    },
+    {
+      id: 2,
+      img: '../img/kiwi.png',
+      title: 'KIWI SHOP',
+      github: 'https://compassionate-wing-2b700d.netlify.app/',
+      link: 'https://github.com/MrDeath22/Kivi-magazin',
+      category: 'react'
+    },
+    {
+      id: 5,
+      img: '../img/music-player.png',
+      title: 'MUSIC PLAYER',
+      github: 'https://github.com/abdusamadweb/Music-Player',
+      link: 'https://musing-neumann-30aaef.netlify.app/',
+      category: 'react'
+    },
+  ],
+  html: [
+    {
+      id: 3,
+      img: '../img/product.jpg',
+      title: 'Product',
+      github: 'https://github.com/abdusamadweb/Product',
+      link: 'https://product-byabdusamad.netlify.app/',
+      category: 'html'
+    },
+    {
+      id: 4,
+      img: '../img/freshio.png',
+      title: 'FRESHIO',
+      github: 'https://github.com/abdusamadweb/Freshio',
+      link: 'https://freshio-byabdusamad.netlify.app/',
+      category: 'html'
+    },
+    {
+      id: 6,
+      img: '../img/fresh2.png',
+      title: 'fresh',
+      github: 'https://github.com/abdusamadweb/Fresh1',
+      link: 'https://fresh1by-abdusamad.netlify.app/',
+      category: 'html'
+    },
+    {
+      id: 7,
+      img: '../img/finswet.jpg',
+      title: 'finswet',
+      github: 'https://github.com/abdusamadweb/finsweet-myteam',
+      link: 'https://finsweet-byabdusamad.netlify.app/',
+      category: 'html'
+    },
+    {
+      id: 8,
+      img: '../img/portfolio.png',
+      title: 'portfolio',
+      github: 'https://github.com/abdusamadweb/Portfolio',
+      link: 'https://portfolio-byabdusamad.netlify.app/',
+      category: 'html'
+    },
+  ],
+  vue: [
+    {
+      id: 9,
+      img: '../img/qashqadaryo.png',
+      title: 'Qashqadaryo',
+      github: 'https://github.com/abdusamadweb/Qashqadaryo-boshqarmasi',
+      link: 'https://qashqadaryo-byabdusamad.netlify.app/',
+      category: 'vue'
+    },
+  ],
+}
---
+++ js/main.js
@@ -0,0 +1,137 @@
+let elMenuBtn = document.querySelector(".menu-btn");
+let elModal = document.querySelector(".nav");
+let elLinethird = document.querySelector(".line2");
+let elLineOne = document.querySelector(".line1");
+let elLineThree = document.querySelector(".line3");
+let elLine = document.querySelectorAll(".experience__line");
+
+elMenuBtn.addEventListener("click", function () {
+  elModal.classList.toggle("grow-height");
+  elLinethird.classList.toggle("remove-line");
+  elLineOne.classList.toggle("rotate-line1");
+  elLineThree.classList.toggle("rotate-line3");
+});
+
+elLine.forEach((item) => {
+  window.addEventListener("scroll", function () {
+    header = document.querySelector("header");
+    header.classList.toggle("header-bg", window.scrollY > 200);
+    item.classList.toggle("float", window.scrollY > 300);
+  });
+});
+
+window.addEventListener('scroll', () => {
+  header = document.querySelector("header");
+  header.classList.toggle("header-bg",window.scrollY > 200);
+})
+
+const links = document.querySelectorAll(".nav__item");
+const sections = document.querySelectorAll("section");
+function changeLinkState() {
+  let index = sections.length;
+
+  while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
+
+  links.forEach((link) => link.classList.remove("nav__link--active"));
+  links[index].classList.add("nav__link--active");
+}
+
+changeLinkState();
+window.addEventListener("scroll", changeLinkState);
+
+var typed = new Typed(".name", {
+  strings: ["Abdusamad", "Coder", "AbuDev", "Creative"],
+  typeSpeed: 80,
+  backSpeed: 80,
+  loop: true,
+});
+
+const skills = document.querySelector('.about__texts__list');
+for(let i of data.skills) {
+  skills.innerHTML += `<li class="about__skill">${i}</li>`
+}
+
+const offers = document.querySelector('.my-offer__list');
+for(let i of data.offers) {
+  offers.innerHTML += `
+<li class="my-offer__item col-md-3 col-sm-6 col-xs-12 offer-box">
+  <div class="offer-block">
+    <div class="bottom-border"></div>
+    <img class="offer-block__img" src="${i.img}" alt="img">
+    <h4 class="offer-box__title">${i.title}</h4>
+    <p class="offer-box__desc">${i.desc}</p>
+  </div>
+</li>`
+}
+
+const awards = document.querySelector('.services__list');
+for(let i of data.awards) {
+  awards.innerHTML += `
+  <li class="services__item projects col-md-3 col-sm-6 col-xs-12">
+    <i class="${i.icon} services__item--before"></i>
+    <span class="projects__count">${i.count}</span>
+    <p class="projects__desc">${i.desc}</p>
+  </li>
+  `
+}
+
+const defTab = data.allproducts;
+const tab1 = document.querySelector('.portfolio__menu-item1');
+const tab2 = document.querySelector('.portfolio__menu-item2');
+const tab3 = document.querySelector('.portfolio__menu-item3');
+const tab4 = document.querySelector('.portfolio__menu-item4');
+tab1.addEventListener('click', () => {
+  setProduct(defTab);
+  tab1.classList.add('portfolio__menu-item--active');
+  tab2.classList.remove('portfolio__menu-item--active');
+  tab3.classList.remove('portfolio__menu-item--active');
+  tab4.classList.remove('portfolio__menu-item--active');
+});
+tab2.addEventListener('click', () => {
+  setProduct(data.react);
+  tab2.classList.add('portfolio__menu-item--active');
+  tab1.classList.remove('portfolio__menu-item--active');
+  tab3.classList.remove('portfolio__menu-item--active');
+  tab4.classList.remove('portfolio__menu-item--active');
+});
+tab3.addEventListener('click', () => {
+  setProduct(data.html);
+  tab3.classList.add('portfolio__menu-item--active');
+  tab1.classList.remove('portfolio__menu-item--active');
+  tab2.classList.remove('portfolio__menu-item--active');
+  tab4.classList.remove('portfolio__menu-item--active');
+});
+tab4.addEventListener('click', () => {
+  setProduct(data.vue);
+  tab4.classList.add('portfolio__menu-item--active');
+  tab1.classList.remove('portfolio__menu-item--active');
+  tab2.classList.remove('portfolio__menu-item--active');
+  tab3.classList.remove('portfolio__menu-item--active');
+});
+
+const products = document.querySelector('.portfolio__list');
+const setProduct = (product) => {
+  products.innerHTML = '';
+  for(let i of product.slice(0, 8)) {
+    products.innerHTML += `
+    <li class="portfolio__item">
+      <div class="portfolio__links">
+        <a href="${i.github}"
+          target="_blank"
+          ><i class="bx bxl-github"></i></a
+        ><a
+          href="${i.link}"
+          class="netlify__link"
+          target="_blank"
+          ><i class="bx bx-globe"></i
+        ></a>
+      </div>
+      <h3 class="portfolio__name">${i.title}</h3>
+      <img class="portfolio__img" src="${i.img}" alt="${i.title}" />
+    </li>
+    `
+  }
+}
+setProduct(defTab);
+
+
